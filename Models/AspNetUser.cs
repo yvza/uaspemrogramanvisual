9e9547c7e3c4fba3ed79822422a7802eefd707f3
Yuza <akhmadyuza@gmail.com>
2022-11-23 13:27:46 +0700

    update, fix primary key not found

--- Models/AspNetUser.cs
+++ Models/AspNetUser.cs
@@ -29,6 +29,7 @@ namespace mvcwithlogin.Models
         public bool LockoutEnabled { get; set; }
         public int AccessFailedCount { get; set; }
 
+        public virtual AspNetPlayerDetail AspNetPlayerDetail { get; set; } = null!;
         public virtual ICollection<AspNetUserClaim> AspNetUserClaims { get; set; }
         public virtual ICollection<AspNetUserLogin> AspNetUserLogins { get; set; }
         public virtual ICollection<AspNetUserToken> AspNetUserTokens { get; set; }
