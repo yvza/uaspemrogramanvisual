98876b0c71a2032ad41040c1e28cec9a91ddab62
Yuza <akhmadyuza@gmail.com>
2022-11-23 13:31:27 +0700

    update, fix primary key not found

--- Models/AspNetPunishmentType.cs
+++ Models/AspNetPunishmentType.cs
@@ -5,7 +5,14 @@ namespace mvcwithlogin.Models
 {
     public partial class AspNetPunishmentType
     {
+        public AspNetPunishmentType()
+        {
+            AspNetHasPunishments = new HashSet<AspNetHasPunishment>();
+        }
+
         public int Id { get; set; }
         public string Type { get; set; } = null!;
+
+        public virtual ICollection<AspNetHasPunishment> AspNetHasPunishments { get; set; }
     }
 }
