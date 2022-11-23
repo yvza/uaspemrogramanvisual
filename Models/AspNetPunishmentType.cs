b7f735e957fe008687cfc3c586751bc3e6aec2fd
Yuza <akhmadyuza@gmail.com>
2022-11-23 08:52:40 +0700

    scaffold exiting db

---
+++ Models/AspNetPunishmentType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mvcwithlogin.Models
+{
+    public partial class AspNetPunishmentType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = null!;
+    }
+}
