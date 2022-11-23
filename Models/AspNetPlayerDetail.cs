95168602b302706907e372259c218e99ed3bd8aa
Yuza <akhmadyuza@gmail.com>
2022-11-23 08:52:32 +0700

    scaffold exiting db

---
+++ Models/AspNetPlayerDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mvcwithlogin.Models
+{
+    public partial class AspNetPlayerDetail
+    {
+        public string UserId { get; set; } = null!;
+        public string HasPunishment { get; set; } = null!;
+
+        public virtual AspNetUser User { get; set; } = null!;
+    }
+}
